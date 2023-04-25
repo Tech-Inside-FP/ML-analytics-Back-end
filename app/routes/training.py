609e82acda73ad3ba7c6f7e47c3880a3f6493b85
MT <msouza.os@hotmail.com>
2023-04-25 17:25:44 -0300

    Creating get route and especifing the type of file parameter of post route

--- app/routes/training.py
+++ app/routes/training.py
@@ -1,10 +1,15 @@
 # imports
+from schemas.training import DatasetSchema
 from fastapi import APIRouter
 
 
 trainRouter = APIRouter()
 
 
+@trainRouter.get(path='/')
+async def root():
+    return {'training-page': 'Running!'}
+
 @trainRouter.post(path='/')
-async def deploy_dataset(file: dict):
+async def deploy_dataset(file: DatasetSchema = (...)):
     pass
