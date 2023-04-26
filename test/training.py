a061f5309d57bb92cf42c9cbb5f82ad343f9301d
MT <msouza.os@hotmail.com>
2023-04-25 23:00:05 -0300

    Refactoring test code

--- test/training.py
+++ test/training.py
@@ -11,8 +11,8 @@ testClient = TestClient(app=app)
 def test_post_request() -> None:
     data = {
         'filename': 'file.csv', 
-        'content': base64.b64encode(s='testing post request')
+        'content': base64.b64encode(s=b'testing post request').decode(encoding='utf-8')
     }
-    response = testClient.post(url='/training', content=data)
+    response = testClient.post(url='/training', json=data)
     assert response.status_code == 200
     assert response.json() == {'response': 'testing post request'}
