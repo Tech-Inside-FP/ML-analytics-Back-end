646d3bd35af736e4f903b1e32f3965dbc737e76a
MT <msouza.os@hotmail.com>
2023-04-25 22:41:22 -0300

    Refactoring code of training route

--- app/routes/training.py
+++ app/routes/training.py
@@ -2,14 +2,21 @@
 from schemas.training import DatasetSchema
 from fastapi import APIRouter
 
+import base64
+
 
 trainRouter = APIRouter()
 
 
 @trainRouter.get(path='/')
-async def root():
-    return {'training-page': 'Running!'}
+async def root() -> dict:
+    return {'training-page': 'Running'}
 
 @trainRouter.post(path='/')
-async def deploy_dataset(file: DatasetSchema = (...)):
-    pass
+async def deploy_dataset(file: DatasetSchema = (...)) -> dict:
+    # extracting content
+    content = file.content
+    # decoding content
+    decode = base64.b64decode(s=content)
+    
+    return {'response': decode.decode(encoding='utf-8')}
