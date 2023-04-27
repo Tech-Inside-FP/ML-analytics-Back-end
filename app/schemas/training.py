3fbe3d36706f95009c91c9cce2a0bdc1d6dbf62e
MT <msouza.os@hotmail.com>
2023-04-27 18:57:06 -0300

    Updating dataset schema

--- app/schemas/training.py
+++ app/schemas/training.py
@@ -1,9 +1,29 @@
 # imports
-from pydantic import BaseModel
-from typing import Optional
+from pydantic import BaseModel, Field
+from typing import Optional, List
+
+import base64
 
 
+# --- MAIN SCHEMA ---
 class DatasetSchema(BaseModel):
 
-    filename: Optional[str] = None
-    content: bytes
+    filename: Optional[str]
+    content: bytes = Field(...)
+    features: List[str] = Field(...)
+    target: str = Field(...)
+    
+    class Config:
+        schema_extra = {
+            'example': {
+                'filename': 'database.csv',
+                'content': base64.b64encode('C:/Users/Matheus/WorkSpace/ML-AnAIytics-Backend/requirements.txt'.encode(encoding='utf-8')),
+                'features': [
+                    'column1',
+                    'column3',
+                    'column4',
+                    'column7'
+                ],
+                'target': 'column8'
+            }
+        }
