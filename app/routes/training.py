6ad7653946bbc836288a566170f90ad7bba848b0
MT <msouza.os@hotmail.com>
2023-05-21 23:00:46 -0300

    create (package): creating script to training mutiple models

--- app/routes/training.py
+++ app/routes/training.py
@@ -1,4 +1,6 @@
 # imports
+from utils.preprocessing import *
+
 from fastapi.encoders import jsonable_encoder
 from schemas.training import DatasetSchema
 from fastapi import APIRouter
@@ -18,10 +20,39 @@ async def read_request(data) -> dict:
     df_bytes = df_base64.decode(encoding='utf-8')
     # converting to DataFrame
     df = pd.read_json(path_or_buf=df_bytes)
+    df_copy = df.copy(deep=True)
+    
+    # dropping nan rows
+    df_copy.dropna(axis=0, inplace=True)
+    
+    # preprocessing methods
+    preprocess_method = {
+        'int32': tratar_dados_numerico,
+        'int64': tratar_dados_numerico,
+        'int128': tratar_dados_numerico,
+        'float32': tratar_dados_numerico,
+        'float64': tratar_dados_numerico,
+        'float128': tratar_dados_numerico,
+        'O': tratar_dados_string,
+        'datetime64': tratar_coluna_data
+    }
+    
+    # collecting columns
+    columns = df_copy.columns
+    column_dtype = df_copy.dtypes
+    remove_col = []
+    for col, dt in zip(columns, column_dtype):
+        dt = str(dt)
+        if dt in ['int32', 'int64', 'float32', 'float64']:
+            df_copy[col] = preprocess_method[dt](df_copy[col])
+        elif dt in ['O', 'datetime64']:
+            df_copy[col] = df_copy[col].map(preprocess_method[dt])
+        else:
+            remove_col.append(col)
     
     return {
         'filename': data['filename'],
-        'dataframe': df,
+        'dataframe': df_copy,
         'features': data['features'],
         'target': data['target']
     }
@@ -37,4 +68,4 @@ async def deploy_dataset(request: DatasetSchema = (...)) -> dict:
     request = jsonable_encoder(obj=request)
     content = await read_request(data=request)
     
-    return {'response': list(content['dataframe'].columns)}
+    return {'response': list(content['dataframe'])}
