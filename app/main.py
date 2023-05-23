9eba101dabccb86f4ac4194ad0a3599ab0a3f8a9
MT <msouza.os@hotmail.com>
2023-05-23 16:14:07 -0300

    update (server): removing line to run the server automatically

--- app/main.py
+++ app/main.py
@@ -12,7 +12,3 @@ app.include_router(router=trainRouter, prefix='/training')
 @app.get(path='/')
 def root():
     return {'ML-AnAIytics': 'Running'}
-
-
-if __name__=='__main__':
-    uvicorn.run(app='main:app', host='0.0.0.0', port=8000, reload=True)
